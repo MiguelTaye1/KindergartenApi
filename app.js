12282d1489cad3475642603d4053c5fcc2f52fa9
migueltaye1 <miguelltaye1@gmail.com>
2025-07-17 01:51:30 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+//This is the entry point for the Kindergarten API application
+const express = require('express');
+const mongoose = require('mongoose');
+//IMPORT dotenv to manage environment variables
+require('dotenv').config();
+//import corse so that the frontend can access the backend
+const cors = require('cors');
+
+
+
+//Create an app based on the express framework
+const app = express();
+//Use cors to allow cross-origin requests
+app.use(cors());
+
+//below we allow our api to parse incoming JSON requests
+app.use(express.json());
+
+//Test/establish the connection to the database using the link from the .env file
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
+
+
+const port = process.env.PORT || 3000;//Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
